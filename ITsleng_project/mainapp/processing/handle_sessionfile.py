fed04079148e36a87a373601e38f6dddb45dc803
TSuvorov <suvorovt@gmail.com>
2023-03-25 14:09:51 +0300

    upd "handle_sessionfile": optimization of write to session files

--- ITsleng_project/mainapp/processing/handle_sessionfile.py
+++ ITsleng_project/mainapp/processing/handle_sessionfile.py
@@ -1,4 +1,6 @@
-from time import time
+import collections
+from time import time, perf_counter
+
 from typing import TypeVar
 
 import rapidjson
@@ -27,8 +29,8 @@ def create_session_file(session_id: str) -> dict:
     random.shuffle(all_qa)
     full_file_path = os.path.join(SESSIONFOLDER, f'{session_id}.json')
     # запись данных JSON в файл
-    with open(full_file_path, "w", encoding="utf-8") as new_session:
-        new_session.write(str(all_qa).replace("'", '"'))
+    with open(full_file_path, "w", encoding="utf-8") as new_session_fp:
+        rapidjson.dump(all_qa, new_session_fp, ensure_ascii=False)
         print(f'Created new session file {session_id[-10:]}')
     return sentences
 
@@ -49,15 +51,13 @@ def get_qa_session_sentence(session_id) -> dict:
     # Читаем содержимое JSON
     with open(full_file_path, 'r', encoding="utf-8") as fp:
         qa_session_sentences: list = rapidjson.load(fp)
-
     # Берет первый в списке вопрос
     qa_session_sentence = qa_session_sentences.pop(0)
     # Записывает его в конец
     qa_session_sentences.append(qa_session_sentence)
-
     # Перезаписываем содержимое
-    with open(full_file_path, "w+", encoding="utf-8") as newfp:
-        newfp.write(str(qa_session_sentences).replace("'", '"'))
+    with open(full_file_path, "w", encoding="utf-8") as newfp:
+        rapidjson.dump(qa_session_sentences, newfp, ensure_ascii=False)
 
     return qa_session_sentence
 
@@ -75,5 +75,8 @@ def remove_sessions_old_files(time_ago):
             os.remove(file)
 
 
+
+
+
 if __name__ == '__main__':
-    remove_sessions_old_files(TIME_1DAY_AGO)
+    get_qa_session_sentence('780b15c4-be75-480c-b30e-1973a43a83df')
